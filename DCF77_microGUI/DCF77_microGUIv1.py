d009f0c08ab6f3c69a52382dca293d1bf6ff68a1
xiansnn <xiansnn@hotmail.com>
2023-02-26 18:06:11 +0100

    minor cosmetic changes

--- DCF77_microGUI/DCF77_microGUIv1.py
+++ DCF77_microGUI/DCF77_microGUIv1.py
@@ -1,6 +1,6 @@
 import hardware_setup
 from gui.core.ugui import Screen, ssd
-from gui.widgets import Label, LED, Dial, Pointer, Button, CloseButton, Textbox
+from gui.widgets import Label, LED, Dial, Pointer, Button, Textbox
 from gui.core.writer import CWriter
 
 # Font for CWriter
@@ -15,9 +15,11 @@ from gui.core.colors import *
 from cmath import rect, pi
 import uasyncio as asyncio
 import time
+from machine import Timer
+
 #-------------------------------
 
-# DEBUG probe definitions
+# DEBUG logic analyser probe definitions
 from debug_utility.pulses import *
 # D0 = Probe(27) # LocalTimeCalendar._timer_IRQ
 # D1 = Probe(16) # DCF_Decoder._DCF_clock_IRQ_handler
@@ -26,7 +28,7 @@ from debug_utility.pulses import *
 # D4 = Probe(19) # DCF_Display_nanoGUI.update_signal_status
 # D5 = Probe(20) # DCF_Display_nanoGUI.update_date_and_time
 # D6 = Probe(21) # _StatusController.time_status == SYNC
-# D7 = Probe(26) # gui.core.nanogui.refresh(ssd)
+# D7 = Probe(26) # -
 
 
 # import DCF modules
@@ -34,25 +36,40 @@ TONE_GPIO = const(7) # the GPIO where DCF signal is received by MCU
 from DCF77.decoder_uGUIv1 import *
 from DCF77.local_time_calendar_uGUI import LocalTimeCalendar
 
-class DCF():
+sensor_temp = machine.ADC(4)
+conversion_factor = 3.3 / 65535
+def temperature():
+    reading = sensor_temp.read_u16() * conversion_factor
+    temperature = 27 - (reading - 0.706)/0.001721
+    return temperature
+
+# while True:
+#     reading = sensor_temp.read_u16() * conversion_factor
+#     
+#     # The temperature sensor measures the Vbe voltage of a biased bipolar diode, connected to the fifth ADC channel
+#     # Typically, Vbe = 0.706V at 27 degrees C, with a slope of -1.721mV (0.001721) per degree. 
+#     temperature = 27 - (reading - 0.706)/0.001721
+#     print(temperature)
+#     utime.sleep(2)
+
+
+class DCF_device():
     def __init__(self):
         self.local_time = LocalTimeCalendar()
         self.dcf_decoder = DCF_Decoder(TONE_GPIO, self.local_time)
         asyncio.create_task(self.dcf_decoder.DCF_signal_monitoring())
         asyncio.create_task(self.dcf_decoder.frame_decoder())
-        asyncio.create_task(self.update())
+        asyncio.create_task(self.clock_update())
         
-    async def update(self):
+    async def clock_update(self):
         while True:
             D7.off()
             await Screen.timer_elapsed.wait()
             D7.on()
             Screen.timer_elapsed.clear()
             self.local_time.next_second()
-    #         print( f"{self.local_time.get_local_time()}\t{self.dcf_decoder.get_time_status()}\t{self.dcf_decoder.get_signal_status()}")
-
         
-    def localtime(self):
+    def get_local_time(self):
 #         localtime : t[0]:year, t[1]:month, t[2]:mday, t[3]:hour, t[4]:minute, t[5]:second, t[6]:weekday, t[7]:time_zone
         return self.local_time.get_raw_time_and_date()
     
@@ -60,56 +77,63 @@ class DCF():
         ts = self.dcf_decoder.get_time_status()
         time_state = ts[1]
         if ts[1] == SYNC:
-            ts_symbol = "sync'd"
+            ts_text = "sync'd"
         elif time_state == SYNC_IN_PROGRESS:
-            ts_symbol = "in progress"
+            ts_text = "in progress"
         elif time_state == SYNC_FAILED:
-            ts_symbol = "frame fail"
+            ts_text = "frame fail"
         elif time_state == OUT_OF_SYNC:
-            ts_symbol = "out of sync"
+            ts_text = "out of sync"
         else:
-            ts_symbol = "init"
+            ts_text = "init"
         ss = self.dcf_decoder.get_signal_status()
         bit_rank = ss[0]
         last_bit = ss[1]
-        return (ts_symbol,bit_rank,last_bit)
+        return (ts_text,bit_rank,last_bit)
 #         time_status = [self._status_controller.time_event, self._status_controller.time_state, self._status_controller.error_message]
 #         signal_status [self._status_controller.last_received_frame_bit_rank, self._status_controller.last_received_frame_bit,
 #                 self._status_controller.signal_event, self._status_controller.signal_state]
         
-    def get_time_status_color(self):
+    def get_time_status_rendering(self):
         status = self.dcf_decoder.get_time_status()
         state = status[1]
         if state == SYNC:
+            blink = False
             color = GREEN
         elif state == SYNC_IN_PROGRESS:
+            blink = True
             color = GREY
         elif state == SYNC_FAILED:
+            blink = True
             color = YELLOW
         elif state == OUT_OF_SYNC:
+            blink = False
             color = RED
         else:
+            blink = True
             color = WHITE
-        return color
-    
-dcf_clock = DCF()
+        return (blink, color)
+
+
+dcf_clock = DCF_device()
 
 
 # triggering mechanism = 1-second internal timer
-from machine import Timer
+
 def timer_IRQ(timer):
     irq_state = machine.disable_irq()
     D0.on()
     Screen.timer_elapsed.set()
     D0.off()
     machine.enable_irq(irq_state)
+
 timer = Timer(mode=Timer.PERIODIC, freq=1, callback=timer_IRQ)
-# Screen._timer_elapsed = asyncio.ThreadSafeFlag() # evolution possible du Screen : prendre en compte ThreadSafeFlag
+
 Screen.timer_elapsed = asyncio.Event() # evolution possible du Screen : prendre en compte ThreadSafeFlag
 
+# conversions table for Calendar
 days   = ('LUN', 'MAR', 'MER', 'JEU', 'VEN', 'SAM', 'DIM')
-months = ('JAN', 'FEV', 'MAR', 'AVR', 'MAY', 'JUN', 'JUL',
-          'AOU', 'SEP', 'OCT', 'NOV', 'DEC')
+months = ('JAN', 'FEV', 'MAR', 'AVR', 'MAY', 'JUN', 'JUL', 'AOU', 'SEP', 'OCT', 'NOV', 'DEC')
 
 
 def fwdbutton(wri, row, col, cls_screen, text='Next'):
@@ -120,7 +144,6 @@ def fwdbutton(wri, row, col, cls_screen, text='Next'):
            fgcolor = YELLOW, bgcolor = BLACK,
            text = text, shape = RECTANGLE)
 
-
 class DCF_clock_screen(Screen):
     def __init__(self):
         super().__init__()
@@ -129,16 +152,27 @@ class DCF_clock_screen(Screen):
                   'bgcolor' : BLACK,
                   'justify' : Label.CENTRE,
           }
+        temp_colors = {'bdcolor' : False,
+                  'fgcolor' : WHITE,
+                  'bgcolor' : BLACK,
+                  'justify' : Label.CENTRE,
+          }
         # verbose default indicates if fast rendering is enabled
-        wri = CWriter(ssd, arial10, YELLOW, BLACK, verbose=False)  # Report on fast mode. Or use verbose=False
-        wri_date = CWriter(ssd, date_font, YELLOW, BLACK, verbose=False)  # Report on fast mode. Or use verbose=False
-        wri_time = CWriter(ssd, hours_font, YELLOW, BLACK, verbose=False)  # Report on fast mode. Or use verbose=False
-        wri_seconds = CWriter(ssd, seconds_font, YELLOW, BLACK, verbose=False)  # Report on fast mode. Or use verbose=False
+        wri         = CWriter(ssd, arial10, YELLOW, BLACK, verbose=False)  
+        wri_date    = CWriter(ssd, date_font, YELLOW, BLACK, verbose=False)  
+        wri_time    = CWriter(ssd, hours_font, YELLOW, BLACK, verbose=False)  
+        wri_seconds = CWriter(ssd, seconds_font, YELLOW, BLACK, verbose=False)  
+        wri_temp    = CWriter(ssd, seconds_font, YELLOW, BLACK, verbose=False)  
         
         gap = 4  # Vertical gap between widgets
         fwdbutton(wri, 4, 80, DCF_detail_screen, text='> detail')
        
         self.dial = Dial(wri, 2, 2, height = 55, ticks = 12, fgcolor = GREEN, pip = GREEN)
+        
+        col = 2 + self.dial.mcol + 3*gap
+        self.lbl_temperature = Label(wri_temp, 30, col, 30, **temp_colors)
+        col = self.lbl_temperature.mcol
+        self.lbl_unit = Label(wri, 30, col, "c", **temp_colors)
         
         row = self.dial.mrow + gap
         self.lbl_date = Label(wri_date, row, 2, 124, **labels)
@@ -160,21 +194,29 @@ class DCF_clock_screen(Screen):
 
         hstart = 0 + 0.7j  # Pointer lengths. Will rotate relative to top.
         mstart = 0 + 1j
-        sstart = 0 + 1j 
+        sstart = 0 + 1j
+        
+        
 
         while True:
-            t = dcf_clock.localtime()
-            color = dcf_clock.get_time_status_color()
+            temp = temperature()
+            self.lbl_temperature.value(f"{temp:3.0f}")
+            t = dcf_clock.get_local_time()
+            blink, color = dcf_clock.get_time_status_rendering()
             # localtime : t[0]:year, t[1]:month, t[2]:mday, t[3]:hour, t[4]:minute, t[5]:second, t[6]:weekday, t[7]:time_zone
             hrs.value(hstart * uv(-t[3] * pi/6 - t[4] * pi / 360), CYAN)
             mins.value(mstart * uv(-t[4] * pi/30), CYAN)
             secs.value(sstart * uv(-t[5] * pi/30), RED)
             self.lbl_tim.value(f"{t[3]:02d}:{t[4]:02d}")
             self.lbl_sec.value(f"{t[5]:02d}")
-            self.lbl_date.value(f"{days[t[6]]} {t[2]} {months[t[1] - 1]}")
-            if t[5]%2==0 : self.led_status(True)
-            else: self.led_status(False)
-            self.led_status.color(color)
+            self.lbl_date.value(f"{days[t[6]-1]} {t[2]} {months[t[1]-1]}")
+            if blink == True:
+                if t[5]%2==0 : self.led_status(True)
+                else: self.led_status(False)
+                self.led_status.color(color)
+            else:
+                self.led_status(True)
+                self.led_status.color(color)
             D3.off()
             await Screen.timer_elapsed.wait()
             D3.on()
@@ -205,9 +247,9 @@ class DCF_detail_screen(Screen):
         
     async def adetail_screen(self):
         while True:
-            t = dcf_clock.localtime()
+            t = dcf_clock.get_local_time()
             # localtime : t[0]:year, t[1]:month, t[2]:mday, t[3]:hour, t[4]:minute, t[5]:second, t[6]:weekday, t[7]:time_zone
-            self.lbl_date.value(f"{days[t[6]]} {t[2]} {months[t[1] - 1]} {t[0]} {t[3]:02d}:{t[4]:02d}")
+            self.lbl_date.value(f"{days[t[6]-1]} {t[2]} {months[t[1]-1]} {t[0]} {t[3]:02d}:{t[4]:02d}")
             ts_symbol,bit_rank,last_bit = dcf_clock.get_status()
             if last_bit == None:
                 last_bit = "x"
@@ -221,9 +263,9 @@ class DCF_detail_screen(Screen):
 #--------------------------------------------------
 
 if __name__ == "__main__":
-    print('main : DCF_clock_screen')
-#     Screen.change(DCF_clock_screen)
-    Screen.change(DCF_detail_screen)
+    print('main program')
+    Screen.change(DCF_clock_screen)
+#     Screen.change(DCF_detail_screen)
 
 
 
--- DCF77_microGUI/local_time_calendar_uGUI.py
+++ DCF77_microGUI/local_time_calendar_uGUI.py
@@ -10,43 +10,33 @@ class LocalTimeCalendar():
     """
     def __init__(self):
         #init conversion tables
-#         self._month_values = ("JAN","FEV","MAR","AVR","MAI","JUN","JUL","AOU","SEP","OCT","NOV","DEC")
-#         self._week_day_values = ("LUN","MAR","MER","JEU","VEN","SAM","DIM")
         self._time_zone_values = (0,2,1) #time_zone_code = 2 => CET => UTC+1, time_zone_code = 1 => CEST => UTC+2
         
         #init local time        
         self.year = 2000
-#         self.month = "xxx" # in (JAN ...DEC)
-        self.month_num = 1 # in (1 ... 12)
-        self.mday = 1 # in (1 ... 31)
-        self.hour = 0 # in (0 ... 23)
-        self.minute = 0 # in (0 ... 59)
-        self.second = 0 # in (0 ... 59)
-#         self.weekday = "xxx" # in (LUN ... DIM)
-        self.weekday_num = 1 # in (1 ... 7)
-        self.time_zone = 0 # UTC +{self.time_zone}
+        self.month_num = 1   # in (1 ... 12)
+        self.mday = 1        # in (1 ... 31)
+        self.hour = 0        # in (0 ... 23)
+        self.minute = 0      # in (0 ... 59)
+        self.second = 0      # in (0 ... 59)
+        self.week_day_num = 1 # in (1 ... 7)
+        self.time_zone = 0   # UTC +{self.time_zone}
  
-#     def get_local_time(self):
-#      # localtime : t[0]:year, t[1]:month, t[2]:mday, t[3]:hour, t[4]:minute, t[5]:second, t[6]:weekday, t[7]:time_zone
-#         clock_update = (self.year, self.month, self.mday, self.hour, self.minute, self.second, self.weekday, self.time_zone)
-#         return clock_update
     def get_raw_time_and_date(self):
      # raw_time_and_date : t[0]:year, t[1]:month, t[2]:mday, t[3]:hour, t[4]:minute, t[5]:second, t[6]:weekday, t[7]:time_zone
-        clock_update = (self.year, self.month_num, self.mday, self.hour, self.minute, self.second, self.weekday_num, self.time_zone)
+        clock_update = (self.year, self.month_num, self.mday, self.hour, self.minute, self.second, self.week_day_num, self.time_zone)
         return clock_update
 
     def sync_time(self, DCF_time_pack):
         """
         When a DCF full correct frame is received, the decoder packs a set of bytes
         and this set is used to sync the local clock
-        received pask : year, month_num, day, week_day_num, hours, minutes, time_zone_num
+        received pack : year, month_num, day, week_day_num, hours, minutes, time_zone_num
         """
         (year, month, day, week_day, hours, minutes, time_zone_code) = ustruct.unpack("6HB",DCF_time_pack)
         self.year = 2000+year
-#         self.month = self._month_values[month-1]
         self.month_num = month
         self.mday = day
-#         self.weekday = self._week_day_values[week_day-1]
         self.week_day_num = week_day
         self.hour = hours
         self.minute = minutes
@@ -104,7 +94,6 @@ if __name__ == "__main__":
             D5.on()
             # raw_time_and_date : t[0]:year, t[1]:month, t[2]:mday, t[3]:hour, t[4]:minute, t[5]:second, t[6]:weekday, t[7]:time_zone
             year, month_num, mday, hour, minute, second, week_day_num, time_zone = clock_update
-#             print(f"{LOCAL_TIME_TAB}LocalTime: {clock_update}")
             print(f"{LOCAL_TIME_TAB}LocalTime: {self._week_day_values[week_day_num-1]:3s} {mday:02d} {self._month_values[month_num-1]:3s} {year:4d} {hour:02d}:{minute:002d}:{second:02d} UTC{time_zone:+01d}")
             D5.off()
 
